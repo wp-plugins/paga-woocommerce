f910364c0ee3b5848cb5cb028d74d168b2503cda
k_uko <k_uko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-01 17:23:21 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/paga-woocommerce/trunk@1036057 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ paga-woocommerce.php
@@ -0,0 +1,516 @@
+<?php
+/*
+	Plugin Name: Paga E-Pay
+	Plugin URI: https://www.mypaga.com/
+	Description: Paga E-Pay Plugin
+	Version: 1.0.0
+	Author: Pagatech Limited
+	Author URI: https://www.mypaga.com/
+	License:  GPL-2.0+
+ 	License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+add_action('plugins_loaded', 'woocommerce_paga_init', 0);
+
+function woocommerce_paga_init() {
+
+	if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
+
+	/**
+ 	 * Gateway class
+ 	 */
+	class WC_Tbz_Paga_Gateway extends WC_Payment_Gateway {
+
+		public function __construct(){
+			global $woocommerce;
+
+			$this->id 				= 'tbz_paga_gateway';
+    			$this->icon 				= apply_filters('woocommerce_paga_icon', plugins_url( 'assets/pay-via-paga.png' , __FILE__ ) );
+			$this->has_fields 			= false;
+        		$this->method_title     		= 'pay with paga';
+        		$this->method_description  		= 'your cash, anytime, anywhere';
+
+			// Load the form fields.
+			$this->init_form_fields();
+
+			// Load the settings.
+			$this->init_settings();
+
+			// Define user set variables
+			$this->title 					= $this->get_option( 'title' );
+			$this->description 				= $this->get_option( 'description' );
+			$this->paga_epay_code	 			= $this->get_option( 'paga_epay_code' );
+			$this->testmode					= $this->get_option( 'testmode' );
+
+			//Actions
+			add_action('woocommerce_receipt_tbz_paga_gateway', array($this, 'receipt_page'));
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+
+			// Payment listener/API hook
+			add_action( 'woocommerce_api_wc_tbz_paga_gateway', array( $this, 'check_paga_response' ) );
+		}
+
+        /**
+         * Admin Panel Options
+         **/
+        public function admin_options(){
+            echo '<h3>Paga Payment Gateway</h3>';
+            echo '<p>Paga Payment Gateway allows you to accept payment on your Woocommerce Powered Store Using Paga</p>';
+            echo '<table class="form-table">';
+            $this->generate_settings_html();
+            echo '</table>';
+        }
+
+
+	    /**
+	     * Initialise Gateway Settings Form Fields
+	    **/
+
+		function init_form_fields(){
+
+			$this->form_fields = array(
+			'enabled' => array(
+							'title' 			=> 	'Enable/Disable',
+							'type' 				=> 	'checkbox',
+							'label' 			=>	'Enable Paga Payment Gateway',
+							'description' 			=> 	'Enable or disable the gateway.',
+                    					'desc_tip'      		=> 	true,
+							'default' 			=> 	'yes'
+						),
+				 'title' => array(
+								'title' 		=> 	'Title',
+								'type' 			=> 	'text',
+								'description' 		=> 	'This controls the title which the user sees during checkout.',
+                    						'desc_tip'      	=> 	false,
+								'default' 		=>  'pay with paga'
+							),
+				'description' => array(
+								'title' 		=> 	'Description',
+								'type' 			=> 	'textarea',
+								'description' 		=> 	'This controls the description which the user sees during checkout.',
+								'default' 		=> 	'your cash, anytime, anywhere'
+							),
+
+				'paga_epay_code' => array(
+								'title' 		=> 	'Paga Merchant Key',
+								'type' 			=> 	'text',
+								'description' 		=> 	'Enter your Paga merchant key here' ,
+								'default' 		=> 	'',
+                    						'desc_tip'     		=> 	false
+							),
+				'return_url' => array(
+								'title' 		=> 	'Return URL',
+								'type' 			=> 	'text',
+								'description' 		=> 	'This URL should be copied and put in the Payment notification URL field under the Merchant Information section in the E-Pay Set-up area under your Paga Merchant account.',
+                    						'desc_tip'      	=> 	false,
+                    						'disabled'		=>  true,
+								'css' 			=> 'min-width: 550px !important; color: #000; background: #fff',
+								'default' 		=>  WC()->api_request_url( 'WC_Tbz_Paga_Gateway' )
+							),
+				'testing' => array(
+								'title'       	=> 'Gateway Testing',
+								'type'        	=> 'title',
+								'description' 	=> '',
+							),
+				'testmode' => array(
+							'title'       		=> 'Test Mode',
+							'type'        		=> 'checkbox',
+							'label'       		=> 'Enable Test Mode',
+							'default'     		=> 'no',
+							'description' 		=> 'Test mode enables you to test payments before going live. <br />If you ready to start receving payment on your site, kindly uncheck this.',
+				)
+			);
+		}
+
+
+
+		/**
+		 * Get Paga Args for passing to Paga
+		**/
+		function get_paga_args( $order ) {
+			global $woocommerce;
+
+			$order_id 			= $order->id;
+
+			$order_total		= $order->get_total();
+			$description       	= "Payment for Order ID: $order_id on ". get_bloginfo('name');
+			$return_url 		= WC()->api_request_url( 'WC_Tbz_Paga_Gateway' );
+
+			if ( 'yes' == $this->testmode ) {
+        		$test = "true";
+			} else {
+				$test = "false";
+			}
+
+			// paga Args
+			$paga_args = array(
+				'description'			=> $description,
+				'subtotal' 			=> $order_total,
+				'invoice'			=> $order_id,
+				'return_url'			=> $return_url,
+				'test' 				=> $test
+			);
+
+			$paga_args = apply_filters( 'woocommerce_paga_args', $paga_args );
+			return $paga_args;
+		}
+
+
+	    /**
+		 * Generate the Paga Payment button link
+	    **/
+	    function generate_paga_form( $order_id ) {
+			global $woocommerce;
+
+			$order = new WC_Order( $order_id );
+
+			$paga_args = $this->get_paga_args( $order );
+
+			$paga_epay_code = $this->paga_epay_code;
+
+			$paga_args_array = array();
+
+			foreach ($paga_args as $key => $value) {
+				$paga_args_array[] = '<input type="hidden" name="'.esc_attr( $key ).'" value="'.esc_attr( $value ).'" />';
+			}
+
+			return '<form action="" method="post" id="paga_payment_form" target="_top">
+					' . implode('', $paga_args_array) . '
+				</form>
+					<!-- begin Paga ePay widget code -->
+					<script type="text/javascript" src="https://www.mypaga.com/paga-web/epay/ePay-button.paga?k='.$paga_epay_code.'&e=false"> </script>
+					<!-- end Paga ePay widget code -->
+					<a class="button cancel" href="'.esc_url( $order->get_cancel_order_url() ).'">'.__('Cancel order &amp; restore cart', 'woocommerce').'</a>'
+				;
+
+		}
+
+
+	    /**
+	     * Process the payment and return the result
+	    **/
+		function process_payment( $order_id ) {
+
+			$order = new WC_Order( $order_id );
+	        return array(
+	        	'result' 	=> 'success',
+				'redirect'	=> $order->get_checkout_payment_url( true )
+	        );
+
+		}
+
+
+	    /**
+	     * Output for the order received page.
+	    **/
+		function receipt_page( $order ) {
+			echo '<p>'.__('Thank you for your order, please click the button below to make payment.', 'woocommerce').'</p>';
+			echo $this->generate_paga_form( $order );
+		}
+
+
+		/**
+		 * Process Payment!
+		**/
+		function check_paga_response( $posted ){
+
+			global $woocommerce;
+
+			if(isset($_POST['status']))
+			{
+				$transaction_status = $_POST['status'];
+
+				switch ($transaction_status) {
+					case 'SUCCESS':
+						$message = 'Payment Transaction was Successfull';
+						break;
+					case 'ERROR_TIMEOUT':
+						$message = 'Payment Transaction Timeout. Try again later.';
+						break;
+					case 'ERROR_INSUFFICIENT_BALANCE':
+						$message = 'Insuccient balance in your account';
+						break;
+					case 'ERROR_INVALID_CUSTOMER_ACCOUNT':
+						$message = 'Invalid Customer Account';
+						break;
+					case 'ERROR_CANCELLED':
+						$message = 'Transaction was cancelled.';
+						break;
+					case 'ERROR_BELOW_MINIMUM':
+						$message = 'The order amount is below the minimum allowed. <br />Contact the merchant.';
+						break;
+					case 'ERROR_ABOVE_MAXINUM':
+						$message = 'The order amount is above the maximum allowed. <br />Contact the merchant.';
+						break;
+					case 'ERROR_AUTHENTICATION':
+						$message= 'Invalid Login Details';
+						break;
+					case 'ERROR_UNKNOWN':
+						$message = 'Transaction Failed. Kindly Try again';
+						break;
+
+					default:
+						$message = 'Transaction Failed. Kindly Try again';
+						break;
+				}
+
+				if($transaction_status == 'SUCCESS')
+				{
+					$transaction_id = $_POST['transaction_id'];
+					$order_id 	= $_POST['invoice'];
+					$order_id 	= (int) $order_id;
+
+	                		$order 		= new WC_Order($order_id);
+	
+			        	$order_total	= $order->get_total();
+
+					$amount_paid 	= $_POST['total'];
+
+					// check if the amount paid is equal to the order amount.
+					if($order_total != $amount_paid)
+					{
+			            //after payment hook
+		                do_action('tbz_paga_woo_after_payment', $transaction);
+
+		                //Update the order status
+						$order->update_status('on-hold', '');
+
+						//Error Note
+						$message = 'Thank you for shopping with us.<br />Your payment transaction was successful, but the amount paid is not the same as the total order amount.<br />Your order is currently on-hold.<br />Kindly contact us for more information regarding your order and payment status.';
+
+						//Add Customer Order Note
+	                    $order->add_order_note($message.'<br />Paga Transaction ID: '.$transaction_id, 1);
+
+	                    //Add Admin Order Note
+	                    $order->add_order_note('This order is currently on hold.<br />Reason: Amount paid is less than the total order amount.<br />Amount Paid was: &#8358; '.$amount.' while the total order amount is: &#8358; '.$posted_amount.'<br />Paga Transaction ID: '.$transaction_id);
+
+						// Reduce stock levels
+						$order->reduce_order_stock();
+
+						// Empty cart
+						WC()->cart->empty_cart();
+
+						if ( function_exists( 'wc_add_notice' ) ) {
+							wc_add_notice( $message, 'error' );
+
+						} else { // WC < 2.1
+							$woocommerce->add_error( $message );
+							$woocommerce->set_messages();
+						}
+					}
+					else
+					{
+		                if($order->status == 'processing')
+		                {
+				            //after payment hook
+			                do_action('tbz_paga_woo_after_payment', $transaction);
+
+		                    $order->add_order_note('Payment Via Paga<br />Transaction ID: '.$transaction_id);
+
+		                    //Add customer order note
+		 					$order->add_order_note("Payment Received.<br />Your order is currently being processed.<br />We will be shipping your order to you soon.< br />Paga Transaction ID: '.$transaction_id", 1);
+
+							// Reduce stock levels
+							$order->reduce_order_stock();
+
+							// Empty cart
+							WC()->cart->empty_cart();
+
+							//Transaction Status Message
+							$status_message = 'Thank you for shopping with us.<br />'.$message.'<br />Your order is currently being processed.';
+
+							if ( function_exists( 'wc_add_notice' ) ) {
+								wc_add_notice( $status_message, 'success' );
+
+							} else { // WC < 2.1
+								$woocommerce->add_message( $status_message );
+								$woocommerce->set_messages();
+							}
+		                }
+		                else
+		                {
+
+		                	if( $order->has_downloadable_item() ){
+
+					            //after payment hook
+				                do_action('tbz_paga_woo_after_payment', $transaction);
+
+								$order->update_status('completed', 'Payment received, your order is now complete.');
+
+			                    $order->add_order_note('Payment Via Paga<br />Transaction ID: '.$transaction_id);
+
+			                    //Add customer order note
+			 					$order->add_order_note("Payment Received.<br />Your order is now complete.<br />We will be shipping your order to you soon.", 1);
+
+								// Reduce stock levels
+								$order->reduce_order_stock();
+
+								// Empty cart
+								WC()->cart->empty_cart();
+
+								//Transaction Status Message
+								$status_message = 'Thank you for shopping with us.<br />'.$message.'<br />Your order is now complete.';
+
+								if ( function_exists( 'wc_add_notice' ) ) {
+									wc_add_notice( $status_message, 'success' );
+
+								} else { // WC < 2.1
+									$woocommerce->add_message( $status_message );
+									$woocommerce->set_messages();
+								}
+							}
+
+							else{
+
+					            //after payment hook
+				                do_action('tbz_paga_woo_after_payment', $transaction);
+
+								$order->update_status('processing', 'Payment received, your order is currently being processed.');
+
+			                    $order->add_order_note('Payment Via Paga<br />Transaction ID: '.$transaction_id);
+
+			                    //Add customer order note
+			 					$order->add_order_note("Payment Received.<br />Your order is currently being processed.<br />We will be shipping your order to you soon.", 1);
+
+								// Reduce stock levels
+								$order->reduce_order_stock();
+
+								// Empty cart
+								WC()->cart->empty_cart();
+
+								//Transaction Status Message
+								$status_message = 'Thank you for shopping with us.<br />'.$message.'<br />Your order is currently being processed.';
+
+								if ( function_exists( 'wc_add_notice' ) ) {
+									wc_add_notice( $status_message, 'success' );
+
+								} else { // WC < 2.1
+									$woocommerce->add_message( $status_message );
+									$woocommerce->set_messages();
+								}
+							}
+
+		                }
+	                }
+				}
+
+	            else
+	            {
+					$status_message =  'Thank you for shopping with us. <br />'.$message;
+
+					if ( function_exists( 'wc_add_notice' ) ) {
+						wc_add_notice( $status_message, 'error' );
+
+					} else { // WC < 2.1
+						$woocommerce->add_error( $status_message );
+						$woocommerce->set_messages();
+					}
+	            }
+			}
+			else
+			{
+				$status_message =  'Thank you for shopping with us. <br />However, the transaction wasn\'t successful, payment wasn\'t recieved.';
+
+				if ( function_exists( 'wc_add_notice' ) ) {
+					wc_add_notice( $status_message, 'error' );
+
+				} else { // WC < 2.1
+					$woocommerce->add_error( $status_message );
+					$woocommerce->set_messages();
+				}
+			}
+
+            $redirect_url = get_permalink( wc_get_page_id('myaccount') );
+            wp_redirect( $redirect_url );
+            exit;
+		}
+	}
+
+	/**
+ 	* Add the Gateway to WooCommerce
+ 	**/
+	function woocommerce_add_paga_gateway($methods) {
+		$methods[] = 'WC_Tbz_Paga_Gateway';
+		return $methods;
+	}
+	add_filter('woocommerce_payment_gateways', 'woocommerce_add_paga_gateway' );
+
+
+	/**
+	 * only add the naira currency and symbol if WC versions is less than 2.1
+	 */
+	if ( version_compare( WOOCOMMERCE_VERSION, "2.1" ) <= 0 ) {
+
+		/**
+		* Add NGN as a currency in WC
+		**/
+		add_filter( 'woocommerce_currencies', 'tbz_add_my_currency' );
+
+		if( ! function_exists( 'tbz_add_my_currency' )){
+			function tbz_add_my_currency( $currencies ) {
+			     $currencies['NGN'] = __( 'Naira', 'woocommerce' );
+			     return $currencies;
+			}
+		}
+
+		/**
+		* Enable the naira currency symbol in WC
+		**/
+
+		add_filter('woocommerce_currency_symbol', 'tbz_add_my_currency_symbol', 10, 2);
+
+		if( ! function_exists( 'tbz_add_my_currency_symbol' ) ){
+			function tbz_add_my_currency_symbol( $currency_symbol, $currency ) {
+			     switch( $currency ) {
+			          case 'NGN': $currency_symbol = '&#8358; '; break;
+			     }
+			     return $currency_symbol;
+			}
+		}
+	}
+
+	/**
+	* Add Settings link to the plugin entry in the plugins menu for WC below 2.1
+	**/
+	if ( version_compare( WOOCOMMERCE_VERSION, "2.1" ) <= 0 ) {
+
+		add_filter('plugin_action_links', 'tbz_paga_plugin_action_links', 10, 2);
+
+		function tbz_paga_plugin_action_links($links, $file) {
+		    static $this_plugin;
+
+		    if (!$this_plugin) {
+		        $this_plugin = plugin_basename(__FILE__);
+		    }
+
+		    if ($file == $this_plugin) {
+		        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=woocommerce_settings&tab=payment_gateways&section=WC_Tbz_Paga_Gateway">Settings</a>';
+		        array_unshift($links, $settings_link);
+		    }
+		    return $links;
+		}
+	}
+	/**
+	* Add Settings link to the plugin entry in the plugins menu for WC 2.1 and above
+	**/
+	else{
+		add_filter('plugin_action_links', 'tbz_paga_plugin_action_links', 10, 2);
+
+		function tbz_paga_plugin_action_links($links, $file) {
+		    static $this_plugin;
+
+		    if (!$this_plugin) {
+		        $this_plugin = plugin_basename(__FILE__);
+		    }
+
+		    if ($file == $this_plugin) {
+		        $settings_link = '<a href="' . get_bloginfo('wpurl') . '/wp-admin/admin.php?page=wc-settings&tab=checkout&section=wc_tbz_paga_gateway">Settings</a>';
+		        array_unshift($links, $settings_link);
+		    }
+		    return $links;
+		}
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,143 @@
+﻿=== Paga Woocommerce Payment Gateway ===
+Contributors: tubiz
+Donate link: http://bosun.me/donate
+Tags: woocommerce, payment gateway, payment gateways, paga, interswitch, verve cards, tubiz plugins, verve, nigeria
+Requires at least: 3.5
+Tested up to: 3.9
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Paga Woocommerce Payment Gateway allows you to accept payment on your Woocommerce store via Visa Cards, Mastercards, Verve Cards and Paga account
+
+
+
+
+== Description ==
+
+This is a Paga payment gateway for Woocommerce.
+
+Paga is a Nigerian Payment Gateway
+
+Paga is a unique online payment processor whose vision is to offer buyers and sellers a secure and easy-to-use means of transacting business online.
+
+Paga allows site owners to receive payment for their goods and services on their website without any setup fee.
+
+To signup for a Paga Merchant account visit their website by clicking [here](https://mypaga.com)
+
+Paga Woocommerce Payment Gateway allows you to accept payment on your Woocommerce store using Nigeria issued Visa Card, Mastercard, Verve Cards and Mypaga accounts.
+
+With this Paga Woocommerce Payment Gateway plugin, you will be able to accept the following payment methods in your shop:
+
+* __MasterCards__
+* __Visa Card__
+* __Verve Cards__
+* __Mypaga Accounts__
+
+= Note =
+
+This plugin is meant to be used by merchants in Nigeria.
+
+= Plugin Features =
+
+*   	__Accept payment__ via Visa Cards, Mastercards,Verve Cards and Paga accounts.
+* 	__Seamless integration__ into the WooCommerce checkout page.
+* 	__Add Naira__ currency symbol. To select it go to go to __WooCommerce > Settings__ from the left hand menu, then click __General__ from the top tab. From __Currency__ select Naira, then click on __Save Changes__ for your changes to be effected.
+
+
+= Suggestions / Feature Request =
+
+If you have suggestions or a new feature request, feel free to get in touch with me via the contact form on my website [here](http://bosun.me/get-in-touch/)
+
+You can also follow me on Twitter! **[@tubiz](http://twitter.com/tubiz)**
+
+
+== Installation ==
+
+= Automatic Installation =
+* 	Login to your WordPress Admin area
+* 	Go to "Plugins > Add New" from the left hand menu
+* 	In the search box type "Paga Woocommerce Payment Gateway"
+*	From the search result you will see "Paga Woocommerce Payment Gateway" click on "Install Now" to install the plugin
+*	A popup window will ask you to confirm your wish to install the Plugin.
+
+= Note: =
+If this is the first time you've installed a WordPress Plugin, you may need to enter the FTP login credential information. If you've installed a Plugin before, it will still have the login information. This information is available through your web server host.
+
+* Click "Proceed" to continue the installation. The resulting installation screen will list the installation as successful or note any problems during the install.
+* If successful, click "Activate Plugin" to activate it, or "Return to Plugin Installer" for further actions.
+
+= Manual Installation =
+1. 	Download the plugin zip file
+2. 	Login to your WordPress Admin. Click on "Plugins > Add New" from the left hand menu.
+3.  	Click on the "Upload" option, then click "Choose File" to select the zip file from your computer. Once selected, press "OK" and press the "Install Now" button.
+4.  	Activate the plugin.
+5. 	Open the settings page for WooCommerce and click the "Payment Gateways," tab.
+6. 	Click on the sub tab for "pay with paga".
+7.	Configure your "Paga Payment Gateway" settings. See below for details.
+
+
+
+= Configure the plugin =
+To configure the plugin, go to __WooCommerce > Settings__ from the left hand menu, then click "Payment Gateways" from the top tab. You should see __"pay with paga"__ as an option at the top of the screen. Click on it to configure the payment gateway.
+
+__*You can select the radio button next to the Paga Payment Gateway from the list of payment gateways available to make it the default gateway.*__
+
+* __Enable/Disable__ - check the box to enable Paga Payment Gateway.
+* __Title__ - allows you to determine what your customers will see this payment option as on the checkout page.
+* __Description__ - controls the message that appears under the payment fields on the checkout page. Here you can list the types of cards you accept.
+* __Paga Merchant Key__  - enter your Paga Merchant Merchant key here, this is gotten from your account page on [Mypaga website](https://mypaga.com).
+* __Return URL__ - This URL should be copied and put in the Payment notification URL field under the Merchant Information section in the E-Pay Set-up area under your Paga Merchant account.
+* __Test Mode__ - Check to enable test mode. Test mode enables you to test payments before going live. If you ready to start receving payment on your site, kindly uncheck this.
+* Click on __Save Changes__ for the changes you made to be effected.
+
+
+
+
+
+== Frequently Asked Questions ==
+
+= What Do I Need To Use The Plugin =
+
+1.	You need to have Woocommerce plugin installed and activated on your WordPress site.
+2.	You need to open a merchant account on [Paga](https://mypaga.com)
+
+
+
+
+== Changelog ==
+
+= 1.0.0 =
+*   First release
+
+
+
+
+
+== Upgrade Notice ==
+
+There is no need to upgrade just yet.
+
+
+
+
+
+
+== Screenshots ==
+
+1. WooCommerce payment gateway setting page
+
+2. Paga Wooocommerce Payment Gateway Setting Page
+
+3. Paga Wooocommerce Payment Gateway method on the checkout page
+
+4. Successful Payment Transaction Message
+
+5. Failed Payment Transaction Declined Message
+
+
+
+
+
+== Other Notes ==
+
